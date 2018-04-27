10327c355a86922196860c2c4badb10ac9917afd
Phreework <phreework@gmail.com>
2018-04-27 22:40:28 +0800

    Create the base

---
+++ GmsIniTools/GmsIniTools/Class/OperIni.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GmsIniTools.Class {
+    public class OperIni {
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string defValue, StringBuilder retvalue, int size, string filePath);
+        //读取ini
+        public static string ReadIni(string section, string key, string noText, string filePath) {
+            if (File.Exists(filePath)) {
+                StringBuilder temp = new StringBuilder(1024);
+                GetPrivateProfileString(section, key, noText, temp, 1024, filePath);
+                return temp.ToString();
+            } else {
+                return String.Empty;
+            }
+        }
+        //写入ini
+        public static bool WriteIni(string section, string key, string value, string filePath) {
+            if (File.Exists(filePath)) {
+                long OpStation = WritePrivateProfileString(section, key, value, filePath);
+                if (OpStation == 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+        //删除节
+        public static long DeleteSection(string section, string filePath) {
+            return WritePrivateProfileString(section, null, null, filePath);
+        }
+        //删除键
+        public static long DeleteKey(string section, string key, string filePath) {
+            return WritePrivateProfileString(section, key, null, filePath);
+        }
+    }
+}
---
+++ GmsIniTools/GmsIniTools/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using GmsIniTools.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GmsIniTools {
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window {
+        
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "文本文件|*.ini";
+            if (dialog.ShowDialog() == true) {
+                OperIni.DeleteSection("1", dialog.FileName);
+                TextBox1.Text = OperIni.ReadIni("1", "peo_id", "wrong", dialog.FileName);
+            }
+
+
+            //OperIni.ReadIni("1", "peo_id", "wrong", dialog.FileName);
+            //OperIni.WriteIni("1", "peo_id", "ljhaha", dialog.FileName);
+            //OperIni.DeleteKey("section", "key1", strFilePath);
+            //OperIni.DeleteSection("section", strFilePath);
+        }
+    }
+}
